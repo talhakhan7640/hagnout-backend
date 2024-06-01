e26652067746db7a448b95effd2aac4b72f90f11
talhakhan7640 <talhakhan7640@gmail.com>
2024-06-01 18:20:23 +0530

    Added delete and update routes for the rooms

--- routes/rooms/roomRoute.js
+++ routes/rooms/roomRoute.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { createRoomController, fetchRoomsController, joinRoomController } from '../../controller/rooms/roomController.js';
+import { createRoomController, fetchRoomsController, joinRoomController, deleteRoomController, leaveRoomController} from '../../controller/rooms/roomController.js';
 
 const roomRouter = express.Router();
 
 roomRouter.post('/create-room', createRoomController);
 roomRouter.post('/search-room', fetchRoomsController);
 roomRouter.post('/join-room', joinRoomController);
+roomRouter.delete('/delete-room/:id', deleteRoomController);
+roomRouter.put('/leave-room', leaveRoomController);
 
 export default roomRouter;
