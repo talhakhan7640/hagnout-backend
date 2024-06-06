e1afe0b1cbec140dc143ea5e567cdbe97db37989
talhakhan7640 <talhakhan7640@gmail.com>
2024-06-07 00:13:25 +0530

    Added fetch rooms route

--- routes/rooms/roomRoute.js
+++ routes/rooms/roomRoute.js
@@ -1,12 +1,13 @@
 import express from 'express';
-import { createRoomController, fetchRoomsController, joinRoomController, deleteRoomController, leaveRoomController} from '../../controller/rooms/roomController.js';
+import { createRoomController, searchRoomsController, joinRoomController, deleteRoomController, leaveRoomController, fetchRoomsController} from '../../controller/rooms/roomController.js';
 
 const roomRouter = express.Router();
 
 roomRouter.post('/create-room', createRoomController);
-roomRouter.post('/search-room', fetchRoomsController);
+roomRouter.post('/search-room', searchRoomsController);
 roomRouter.post('/join-room', joinRoomController);
 roomRouter.delete('/delete-room/:id', deleteRoomController);
 roomRouter.put('/leave-room', leaveRoomController);
+roomRouter.post('/fetch-rooms', fetchRoomsController);
 
 export default roomRouter;
