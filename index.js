015086ad7b077f80c9eab8865453422521ac5225
talhakhan7640 <talhakhan7640@gmail.com>
2024-06-25 11:17:42 +0530

    Added message middleware

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
-import express from 'express';
 import mongoose from "mongoose";
-import dotenv from "dotenv";
+import {server} from './server.js';
 import bodyParser from 'body-parser';
 import cors from "cors";
+import express from 'express'
 
 // getting routes
 import userRouter from './routes/users/userRoutes.js';
 import roomRouter from './routes/rooms/roomRoute.js';
+import messageRouter from "./routes/messages/messages.js";
 
-dotenv.config();
-const app = express();
+import { app } from "./server.js";
+
 app.use(cors());
+app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 // use routes
 app.use('/users', userRouter);
 app.use('/rooms', roomRouter);
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
+app.use('/messages', messageRouter);
 
 try { 
     mongoose.connect(process.env.URI)
     .then(() => {
-        app.listen(5000, () => {
+        server.listen(5000, () => {
             console.log(`server started`)
         })
     }).catch((error) => {
