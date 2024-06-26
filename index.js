720b63e7c0b907c1ac93004bea51324c524f89cd
talhakhan7640 <talhakhan7640@gmail.com>
2024-06-26 19:55:12 +0530

    Enabled CORS for frontend

--- index.js
+++ index.js
@@ -21,6 +21,13 @@ app.use('/users', userRouter);
 app.use('/rooms', roomRouter);
 app.use('/messages', messageRouter);
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type ,Accept");
+    next();
+})
+
 try { 
     mongoose.connect(process.env.URI)
     .then(() => {
