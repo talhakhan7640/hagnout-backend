f15cb38b334695a2157b12b2d61ff21354c67836
talhakhan7640 <talhakhan7640@gmail.com>
2024-06-26 20:13:44 +0530

    Enabled CORS

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const corsOptions = {
     origin: "http://localhost/3000"
 }
 app.use(cors(corsOptions));
-app.use(express.json());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
