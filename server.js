2c4d56221bf06f189bc2d37ea764bd3f2e1ec509
talhakhan7640 <talhakhan7640@gmail.com>
2024-07-07 23:34:08 +0530

    added CORS for socket.io

--- server.js
+++ server.js
@@ -11,7 +11,10 @@ const server = http.createServer(app);
 // create an io instance 
 const io = new Server(server, {
 	cors: {
-		origin: "http://localhost:3000"
+		origin: "*:*",
+		 allowedHeaders: ["my-custom-header"],
+		 methods: ["GET", "POST"],
+
 	}
 });
 
