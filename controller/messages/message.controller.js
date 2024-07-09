2dacd2ef434a709814988a20c6ff4e7bbe94abe3
talhakhan7640 <talhakhan7640@gmail.com>
2024-07-09 20:35:35 +0530

    made a few changes

--- controller/messages/message.controller.js
+++ controller/messages/message.controller.js
@@ -3,61 +3,67 @@ import roomModel from "../../models/rooms/roomModel.js";
 import userModel from "../../models/users/userModel.js";
 
 export const getMessages = async (req, res) => {
-  const id = req.params.id;
+	const id = req.params.id;
 
-  try {
-    console.log(id);
-    const room = await roomModel.findById(id);
+	try {
+		console.log(id);
+		const room = await roomModel.findById(id);
 
-    if (!room) {
-      return res.status(404).send({ message: "Room not found" });
-    }
+		if (!room) {
+			return res.status(404).send({ message: "Room not found" });
+		}
 
-    if (room.conversations) {
-      const conversations = [];
+		if (room.conversations) {
+			const conversations = [];
 
-      for (const c of room.conversations) {
-        const user = await userModel.findById(c.senderId.toString());
-        if (user) {
-          conversations.push({
-			file: c.file,
-            messageContent: c.messageContent,
-            username: user.username,
-            profile: user.profilePic
-          });
-        }
-      }
+			for (const c of room.conversations) {
+				const user = await userModel.findById(c.senderId.toString());
+				if (user) {
+					conversations.push({
+						file: c.file,
+						messageContent: c.messageContent,
+						username: user.username,
+						profile: user.profilePic
+					});
+				}
+			}
 
-      return res.send(conversations);
-    } else {
-      return res.send([]);
-    }
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send({ message: "Internal server error" });
-  }
+			return res.send(conversations);
+		} else {
+			return res.send([]);
+		}
+	} catch (error) {
+		console.error(error);
+		return res.status(500).send({ message: "Internal server error" });
+	}
 };
 
 export const sendMessage = async (req, res) => {
-  const messageContent = req.body.messageContent;
-  const { senderId } = req.body;
-  const { roomId } = req.body;
+	const messageContent = req.body.messageContent;
+	const { senderId } = req.body;
+	const { roomId } = req.body;
 	const {file} = req.body;
 
- try {
-    const room = await roomModel.findById(roomId);
+	console.log(messageContent)
+	console.log(senderId)
+	console.log(roomId)
+	console.log(file)
 
-    if (!room) {
-      return res.status(404).send({ message: "Room not found" });
-    }
 
-    room.conversations.push({ messageContent, file,  senderId, roomId });
+	try {
+		const room = await roomModel.findById(roomId);
 
-    await room.save();
+		if (!room) {
+			return res.status(404).send({ message: "Room not found" });
+		}
 
-    res.status(201).send({ message: "Message sent" });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send({ message: "Internal server error" });
-  } 
+		room.conversations.push({ messageContent, file,  senderId, roomId });
+
+		await room.save();
+
+		res.status(201).send({ message: "Message sent" });
+	} catch (error) {
+		console.error(error);
+		res.status(500).send({ message: "Internal server error" });
+	} 
 };
