f4cd87cb36fdfca80aaa25ed6214da0e320ada03
talhakhan7640 <talhakhan7640@gmail.com>
2024-07-14 23:42:45 +0530

    added real time timetamps when the messages was emmited

--- socket/messages.socket.js
+++ socket/messages.socket.js
@@ -1,9 +1,9 @@
 import userModel from "../models/users/userModel.js";
 import { io } from "../server.js";
+import moment from 'moment';
 
 export const realTimeMessaging = async (msgC) => {
    const {messageContent, username, fileUrl} = msgC;
-    // console.log("fuck", msgC)
    const user = await userModel.findOne({username: username});
    console.log(user)
    io.emit("msg", 
@@ -12,6 +12,7 @@ export const realTimeMessaging = async (msgC) => {
        fileUrl: fileUrl,
        username: user.username,
        profilePic: user.profilePic,
+       timestamp: moment().format('MMMM Do YYYY, h:mm:ss a'),
      },
    );
 }
