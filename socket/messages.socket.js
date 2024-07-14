5261a0c7e347ed79bf02576e0ebff697b47cda9a
talhakhan7640 <talhakhan7640@gmail.com>
2024-07-14 16:21:12 +0530

    configured socket to handle files

--- socket/messages.socket.js
+++ socket/messages.socket.js
@@ -2,13 +2,14 @@ import userModel from "../models/users/userModel.js";
 import { io } from "../server.js";
 
 export const realTimeMessaging = async (msgC) => {
-   const {messageContent, username} = msgC;
+   const {messageContent, username, fileUrl} = msgC;
     // console.log("fuck", msgC)
    const user = await userModel.findOne({username: username});
    console.log(user)
    io.emit("msg", 
      {
        messageContent: messageContent,
+       fileUrl: fileUrl,
        username: user.username,
        profilePic: user.profilePic,
      },
