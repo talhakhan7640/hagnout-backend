2b5ada9f4a2548e7e55116e43397fcd7eaaef46a
talhakhan7640 <talhakhan7640@gmail.com>
2024-07-17 16:39:02 +0530

    added user logout functionality

--- controller/users/userController.js
+++ controller/users/userController.js
@@ -141,3 +141,7 @@ export const userLoginController = async (request, response) => {
       });
     });
 };
+
+export const userLogoutController = (req, res) => {
+   res.send({token: ""});
+}
--- routes/users/userRoutes.js
+++ routes/users/userRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { userSignupController, userLoginController } from '../../controller/users/userController.js';
+import { userSignupController, userLoginController, userLogoutController } from '../../controller/users/userController.js';
 const userRouter = express.Router();
 
 userRouter.post('/signup', userSignupController);
 userRouter.post('/login', userLoginController);
+userRouter.get('/logout', userLogoutController);
 
 export default userRouter;
