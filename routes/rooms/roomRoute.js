c1986292c91dd507ab02c51855f2bb7365387ce6
talhakhan7640 <talhakhan7640@gmail.com>
2024-08-15 17:28:03 +0530

    Created add track route for adding tracks in room

--- routes/rooms/roomRoute.js
+++ routes/rooms/roomRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createRoomController, searchRoomsController, joinRoomController, deleteRoomController, leaveRoomController, fetchRoomsController} from '../../controller/rooms/roomController.js';
+import { createRoomController, searchRoomsController, joinRoomController, deleteRoomController, leaveRoomController, fetchRoomsController, addMusicToRoomPlayer} from '../../controller/rooms/roomController.js';
 
 const roomRouter = express.Router();
 
@@ -9,5 +9,6 @@ roomRouter.post('/join-room', joinRoomController);
 roomRouter.delete('/delete-room/:id', deleteRoomController);
 roomRouter.put('/leave-room', leaveRoomController);
 roomRouter.post('/fetch-rooms', fetchRoomsController);
+roomRouter.post('/add-track', addMusicToRoomPlayer);
 
 export default roomRouter;
