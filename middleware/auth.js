f37a02a9111e1bdc97aa35fe5cf082e7de31b4bf
talhakhan7640 <talhakhan7640@gmail.com>
2025-02-10 16:18:02 +0530

    Added authentication middleware to verify JWT tokens

---
+++ middleware/auth.js
@@ -0,0 +1,33 @@
+import jwt from 'jsonwebtoken';
+import userModel from '../models/users/userModel.js';
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (!token) {
+        console.log("⛔ No token provided");
+        return res.status(401).json({ message: "Access Denied: No token provided"});
+    }
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async (err, user) => {
+        if(err) {
+            return res.status(403).json({message: "Invalid token"});
+        }
+
+        const authenticateUser = await userModel.findOne({username :req.body.username });
+
+        if(authenticateUser) {
+            if(authenticateUser._id != user.userId) {
+                console.log("⚠️ Warning: Unauthorized Access Attempted!");
+                return res.status(401).json({ message: "Access Denied: Unauthorized access attempted"});
+            }
+        }
+
+        console.log("✅ Token Verified, User:", user);
+        req.user = user;
+        next();
+    });
+}
+
+export default authenticateToken;
