7277f53c210a8b52b56eecd94ebf974527cdb3b5
talhakhan7640 <talhakhan7640@gmail.com>
2025-03-26 23:05:02 +0530

    configured emit messages in specified socket room

--- socket/messages.socket.js
+++ socket/messages.socket.js
@@ -1,31 +1,39 @@
+import { RedisSearchLanguages } from "redis";
 import userModel from "../models/users/userModel.js";
 import { io } from "../server.js";
 
-var currentdate = new Date(); 
-var datetime = "Last Sync: " + currentdate.getDate() + "/"
-  + (currentdate.getMonth()+1)  + "/" 
-  + currentdate.getFullYear() + " @ "  
-  + currentdate.getHours() + ":"  
-  + currentdate.getMinutes() + ":" 
-  + currentdate.getSeconds();
+var currentdate = new Date();
+var datetime =
+  "Last Sync: " +
+  currentdate.getDate() +
+  "/" +
+  (currentdate.getMonth() + 1) +
+  "/" +
+  currentdate.getFullYear() +
+  " @ " +
+  currentdate.getHours() +
+  ":" +
+  currentdate.getMinutes() +
+  ":" +
+  currentdate.getSeconds();
 
-export const realTimeMessaging = async (msgC) => {
-  const {messageContent, username, fileUrl} = msgC;
-  const user = await userModel.findOne({username: username});
-  io.emit("msg", 
-    {
-      messageContent: messageContent,
-      fileUrl: fileUrl,
-      username: user.username,
-      profilePic: user.profilePic,
-      timestamp: datetime,
-    },
-  );
-}
+export const broadcastMessage = async (roomId, message, username) => {
+  console.log(message);
+  const { messageContent, fileUrl } = message;
+  const user = await userModel.findOne({ username: username });
+  io.to(roomId).emit("receive-message", {
+    messageContent: messageContent,
+    fileUrl: fileUrl,
+    username: username,
+    profilePic: user.profilePic,
+    timestamp: datetime,
+  });
+};
 
 export const realTimeTrackBroadcast = async (trackDetails) => {
   console.log(trackDetails);
-  const {trackName, trackUrl} = trackDetails;
-  io.emit('trackDetails', { trackName, trackUrl }); // Broadcast to all clients
+  const { trackName, trackUrl } = trackDetails;
+  io.emit("trackDetails", { trackName, trackUrl }); // Broadcast to all clients
   // console.log("requested track details: ", trackName, trackUrl);
-}
+  RedisSearchLanguages;
+};
